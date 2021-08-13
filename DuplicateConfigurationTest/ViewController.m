9c650aa8d243dd97e6c135cb1452dc817b76129e
Jayehuang <jianhuangbest@gmail.com>
2021-08-13 21:39:26 +0800

    settings.bundle ， md文档

--- DuplicateConfigurationTest/ViewController.m
+++ DuplicateConfigurationTest/ViewController.m
@@ -6,7 +6,6 @@
 //
 
 #import "ViewController.h"
-#import "AxLocationTool.h"
 
 @interface ViewController ()
 @property (weak, nonatomic) IBOutlet UILabel *settingsInfoLabel;
@@ -38,11 +37,6 @@
             break;
     }
     
-    [[AxLocationTool shareInstance]startLocationWithBlock:^(BOOL isSuccess, AxLocationModel * _Nonnull locationModel) {
-        
-    }];
-    
-    
 }
 -(NSString*) getLocalAppVersion
 {
@@ -52,18 +46,22 @@
     NSUserDefaults *userDefaults = [NSUserDefaults standardUserDefaults];
     
     NSLog(@"%ld",[[userDefaults objectForKey:@"isON"]integerValue]);
-    NSLog(@"%@",[userDefaults objectForKey:@"rank"]);
+    NSLog(@"%@",[userDefaults objectForKey:@"config"]);
     NSLog(@"%@",[userDefaults objectForKey:@"version"]);
     NSLog(@"%@",[userDefaults objectForKey:@"nametext"]);
     NSLog(@"%@",[userDefaults objectForKey:@"passwordText"]);
     NSLog(@"%@",[userDefaults objectForKey:@"slider"]);
     
-    self.settingsInfoLabel.text = [NSString stringWithFormat:@"isON:%ld\nrank:%@\nversion:%@\nnametext:%@\npasswordText:%@\nslider:%@",[[userDefaults objectForKey:@"isON"]integerValue],[userDefaults objectForKey:@"rank"],[userDefaults objectForKey:@"version"],[userDefaults objectForKey:@"nametext"],[userDefaults objectForKey:@"passwordText"],[userDefaults objectForKey:@"slider"]];
+    self.settingsInfoLabel.text = [NSString stringWithFormat:@"isON:%ld\nconfig:%@\nversion:%@\nnametext:%@\npasswordText:%@\nslider:%@",[[userDefaults objectForKey:@"isON"]integerValue],[userDefaults objectForKey:@"config"],[userDefaults objectForKey:@"version"],[userDefaults objectForKey:@"nametext"],[userDefaults objectForKey:@"passwordText"],[userDefaults objectForKey:@"slider"]];
     
-    if (![userDefaults objectForKey:@"rank"]) {
-         [userDefaults setObject:@"开发Value" forKey:@"rank"];
+    if (![userDefaults objectForKey:@"config"]) {
+         [userDefaults setObject:@"开发环境" forKey:@"config"];
     }
-    ///也可设值
+    /*运行应用后直接直接通过上面的代码获取信息，你会得到null；原因是：
+     root.plist实际上只是一个静态文件用来在设置里显示；只有当你对它进行修改，
+     它才会往NSUserDefaults里添加。修改配置后，NSUserDefaults的优先级高于root.plist文件。
+     */
+    ///也可设值 , 首次打开app没有值可以设一下
     [userDefaults setObject:@"2.0.0" forKey:@"version"];
 }
 
